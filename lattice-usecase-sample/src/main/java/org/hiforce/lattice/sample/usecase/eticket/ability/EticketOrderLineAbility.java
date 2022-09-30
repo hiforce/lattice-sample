c3891b24f3b24d756bd349cf2b8b3a893fbd872c
rocky <rocky@quadtalent.com>
2022-10-01 03:24:37 +0800

    update version.

--- lattice-usecase-sample/src/main/java/org/hiforce/lattice/sample/usecase/eticket/ability/EticketOrderLineAbility.java
+++ lattice-usecase-sample/src/main/java/org/hiforce/lattice/sample/usecase/eticket/ability/EticketOrderLineAbility.java
@@ -1,5 +1,6 @@
 package org.hiforce.lattice.sample.usecase.eticket.ability;
 
+import org.hifforce.lattice.annotation.Ability;
 import org.hiforce.lattice.runtime.ability.BaseLatticeAbility;
 import org.hiforce.lattice.runtime.ability.reduce.Reducers;
 import org.hiforce.lattice.sample.usecase.eticket.model.ETicketOrderLine;
@@ -11,6 +12,7 @@ import static org.hiforce.lattice.sample.usecase.eticket.sdk.ETicketTradeSDK.EXT
  * @author Rocky Yu
  * @since 2022/9/28
  */
+@Ability(name = "EticketOrderLineAbility")
 public class EticketOrderLineAbility extends BaseLatticeAbility<BlankETicketTradeSDK> {
 
     public EticketOrderLineAbility(ETicketOrderLine orderLine) {
